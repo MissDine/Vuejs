1fb8f93366220cca599e6eb859b170b4c1b2104b
MissDine <enidnyatichi1212@gmail.com>
2022-07-21 22:02:41 +0300

    Feature edit

---
+++ vue.js
@@ -0,0 +1,5 @@
+// Its a frontend javascript library, used to create dynamic, data - driven and singlepage web applications.
+can also be used to create stand-alone widgets eg searchbar, contact form, sign up etc
+vue is used to create a whole website with multiple pages and components. 
+The websites are normally single pages but all routing is done on the browser and not the server.
+
