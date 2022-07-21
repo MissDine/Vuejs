3ddde21728b203a2cd8a34c7b27e320a650e92f8
MissDine <enidnyatichi1212@gmail.com>
2022-07-21 22:27:01 +0300

    Feature edit

--- vue.js
+++ vue.js
@@ -1,5 +1,17 @@
-// Its a frontend javascript library, used to create dynamic, data - driven and singlepage web applications.
+// -Its a frontend javascript library, used to create dynamic, data - driven and singlepage web applications.
 can also be used to create stand-alone widgets eg searchbar, contact form, sign up etc
-vue is used to create a whole website with multiple pages and components. 
-The websites are normally single pages but all routing is done on the browser and not the server.
+-vue is used to create a whole website with multiple pages and components. 
+-The websites are normally single pages but all routing is done on the browser and not the server.
+-single paged have one html, they are faster because vue gets to render this other widgets or componets on the server.This results in a much faster and smooth website.
+ADVANTAGES /NEW FEATURES IN VUEJS3
+-The composition api-improves reorganization,readability and reusability by giving us access to a setup function inside components.
+-We can now have multible root elements in a component.
+-Teleport- enables us to to render a component in a completely diferent part of the dom.This is useful for things like the modals.
+-Suspense component-enables us handle asynchronous components easily and provide fall-back content until data is loaded.
+-typescript support
+-improved reactivity
+-performance gains
+-multiple v-models especially for custom components
+
+
 
